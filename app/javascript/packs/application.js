d7294baf5dc359254e6eda0cae0245e973a87acc
Maria <maria.altyeva@gmail.com>
2019-11-27 10:27:20 +0800

    autocomplete

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,3 +3,7 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 
 import { initMapbox } from '../plugins/init_mapbox.js';
 initMapbox();
+
+import { initAutocomplete } from '../plugins/init_autocomplete.js';
+
+initAutocomplete();
--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -9,15 +9,24 @@ const initMapbox = () => {
 	const mapElement = document.getElementById("map");
   if (mapElement) {
     const markers = JSON.parse(mapElement.dataset.markers);
- 
+
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v10'
+      style: 'mapbox://styles/maltyeva/ck3gnmpux0a8a1cqndqmn5m9y'
     });
     markers.forEach((marker) => {
-    new mapboxgl.Marker()
+      const element = document.createElement('div');
+      element.className = 'marker';
+      element.style.backgroundImage = `url('${marker.image_url}')`;
+      element.style.backgroundSize = 'contain';
+      element.style.width = '25px';
+      element.style.height = '25px';
+      const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // add this
+
+    new mapboxgl.Marker(element)
       .setLngLat([ marker.lng, marker.lat ])
+      .setPopup(popup)
       .addTo(map);
   });
     fitMapToMarkers(map, markers)
