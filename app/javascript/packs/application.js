8ed5db83c97373c13de42885a55c7a344f816440
Maria <maria.altyeva@gmail.com>
2020-02-26 16:49:12 +0900

    added geocoding

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,3 +3,14 @@ require("@rails/activestorage").start()
 require("channels")
 
 import "bootstrap";
+
+import 'mapbox-gl/dist/mapbox-gl.css'; 
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+
+
+import { initMapbox } from '../plugins/init_mapbox';
+import { initAutocomplete } from '../plugins/init_autocomplete';
+initMapbox();
+initAutocomplete();
+
+// import 'mapbox';
